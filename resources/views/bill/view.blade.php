2980ae8905d43aab8efd0e85d5a2d03f4fec8acf
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2022-11-16 14:29:33 +0530

    app

--- resources/views/bill/view.blade.php
+++ resources/views/bill/view.blade.php
@@ -10,13 +10,7 @@
                 </div>
                 <div class="card-content">
                     <div class="card-body">
-                        <div class="form-group mb-2">
-                            <select class="form-control form-control-sm">                   
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
-                        </div>
+                       
 
                     <img src="{{ asset('storage/' . $bill->photo) }}" class="img-thumbnail" alt="bill">
                     </div>
