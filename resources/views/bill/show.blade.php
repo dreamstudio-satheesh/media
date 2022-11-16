37a5a5092033ff91fa85890eb802cbea3a1a33e6
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2022-11-16 14:23:15 +0530

    app

--- resources/views/bill/show.blade.php
+++ resources/views/bill/show.blade.php
@@ -10,8 +10,18 @@
         </div>
         <div class="card-content">
             <div class="card-body">
-            
-
+                <div class="row">
+                    <div class="col">
+                    <select class="form-control">                   
+                        <option {{ ( $bill->status == 'pending') ? 'selected' : '' }}>pending</option>
+                        <option {{ ( $bill->status == 'approved') ? 'selected' : '' }}>approved</option>
+                        <option {{ ( $bill->status == 'rejected') ? 'selected' : '' }}>rejected</option>
+                    </select>
+                    </div>
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
             <img src="{{ asset('storage/' . $bill->photo) }}" class="img-thumbnail" alt="Cinque Terre">
             </div>
         
--- resources/views/bill/view.blade.php
+++ resources/views/bill/view.blade.php
@@ -3,20 +3,26 @@
 
 @section('content')
 
-<div class="col-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4 class="card-title">Bill Status -{{ $bill->status }}</h4>
-                                </div>
-                                <div class="card-content">
-                                    <div class="card-body">
-                                       
-
-                                       <img src="{{ asset('storage/' . $bill->photo) }}" class="img-thumbnail" alt="Cinque Terre">
-                                    </div>
-                                   
-                                    
-                                </div>
-                            </div>
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Bill Status -{{ $bill->status }}</h4>
+                </div>
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="form-group mb-2">
+                            <select class="form-control form-control-sm">                   
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
                         </div>
+
+                    <img src="{{ asset('storage/' . $bill->photo) }}" class="img-thumbnail" alt="bill">
+                    </div>
+                
+                    
+                </div>
+            </div>
+        </div>
 @endsection
